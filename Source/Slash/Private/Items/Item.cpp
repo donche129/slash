7d37594c69a6db5fb01c2b41c220f325b21496a8
Donald Cherestal <donaldcherestal@gmail.com>
2024-05-03 20:27:51 -0400

    Lecture 44 Drawing Debug Spheres

--- Source/Slash/Private/Items/Item.cpp
+++ Source/Slash/Private/Items/Item.cpp
@@ -2,6 +2,10 @@
 
 
 #include "Items/Item.h"
+#include "DrawDebugHelpers.h"
+#include "Slash/Slash.h"
+
+#define THIRTY 30
 
 // Sets default values
 AItem::AItem()
@@ -16,6 +20,17 @@ void AItem::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	UE_LOG(LogTemp, Warning, TEXT("Begin Play called!"));
+
+	if (GEngine) {
+		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Cyan, FString("Item OnScreen Message!"));
+	}
+
+	UWorld* World = GetWorld();
+
+	FVector Location = GetActorLocation();
+	DRAW_SPHERE(Location)
+	
 }
 
 // Called every frame
@@ -23,5 +38,14 @@ void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	UE_LOG(LogTemp, Warning, TEXT("DeltaTime: %f"), DeltaTime);
+
+	if (GEngine) {
+		FString Name = GetName();
+		FString Message = FString::Printf(TEXT("Item Name: %s"), *Name);
+		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Cyan, Message);
+
+		UE_LOG(LogTemp, Warning, TEXT("Item Name: %s"), *Name);
+	}
 }
 
