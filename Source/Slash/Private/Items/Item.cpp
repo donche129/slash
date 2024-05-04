da288ab5011e7fab75b529e9e5fdd84ba4c02b24
Donald Cherestal <donaldcherestal@gmail.com>
2024-05-04 16:21:08 -0400

    Lecture 50: Actor World Offset

--- Source/Slash/Private/Items/Item.cpp
+++ Source/Slash/Private/Items/Item.cpp
@@ -17,20 +17,21 @@ void AItem::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UWorld* World = GetWorld();
-	FVector Location = GetActorLocation();
-	FVector Forward = GetActorForwardVector();
-	
-	DRAW_SPHERE(Location);
-	// DRAW_LINE(Location, Location + Forward * 100.f);
-	// DRAW_POINT(Location + Forward * 100.f);
-	DRAW_VECTOR(Location, Location + Forward * 100.f)
-
 }
 
 // Called every frame
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	// Movement rate in units of cm/s 
+	float MovementRate = 50.f;
+	float RotationRate = 45.f;
+
+	// MovementRate * DeltaTime = (cm/s) * (s/frame) = (cm/frame)
+	AddActorWorldOffset(FVector(MovementRate * DeltaTime, 0.f, 0.f));
+	AddActorWorldRotation(FRotator(0.f, RotationRate * DeltaTime, 0.f));
+	DRAW_SPHERE_SINGLE_FRAME(GetActorLocation());
+	DRAW_VECTOR_SINGLE_FRAME(GetActorLocation(), GetActorLocation() + GetActorForwardVector() * 100.f);
 }
 
