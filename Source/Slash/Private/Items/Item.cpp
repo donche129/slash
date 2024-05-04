8bb07d18e44a1ac41488c7760666feb41771f507
Donald Cherestal <donaldcherestal@gmail.com>
2024-05-04 02:11:54 -0400

    Section 5: The Actor Class

--- Source/Slash/Private/Items/Item.cpp
+++ Source/Slash/Private/Items/Item.cpp
@@ -2,10 +2,7 @@
 
 
 #include "Items/Item.h"
-#include "DrawDebugHelpers.h"
-#include "Slash/Slash.h"
-
-#define THIRTY 30
+#include "Slash/DebugMacros.h"
 
 // Sets default values
 AItem::AItem()
@@ -19,33 +16,21 @@ AItem::AItem()
 void AItem::BeginPlay()
 {
 	Super::BeginPlay();
-	
-	UE_LOG(LogTemp, Warning, TEXT("Begin Play called!"));
-
-	if (GEngine) {
-		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Cyan, FString("Item OnScreen Message!"));
-	}
 
 	UWorld* World = GetWorld();
-
 	FVector Location = GetActorLocation();
-	DRAW_SPHERE(Location)
+	FVector Forward = GetActorForwardVector();
 	
+	DRAW_SPHERE(Location);
+	// DRAW_LINE(Location, Location + Forward * 100.f);
+	// DRAW_POINT(Location + Forward * 100.f);
+	DRAW_VECTOR(Location, Location + Forward * 100.f)
+
 }
 
 // Called every frame
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
-	UE_LOG(LogTemp, Warning, TEXT("DeltaTime: %f"), DeltaTime);
-
-	if (GEngine) {
-		FString Name = GetName();
-		FString Message = FString::Printf(TEXT("Item Name: %s"), *Name);
-		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Cyan, Message);
-
-		UE_LOG(LogTemp, Warning, TEXT("Item Name: %s"), *Name);
-	}
 }
 
