c92acb333b749d91ee2d76003cb4c0ff783ab7c3
Donald Cherestal <donaldcherestal@gmail.com>
2024-05-05 17:53:33 -0400

    Lecture 57: Template Functions

--- Source/Slash/Private/Items/Item.cpp
+++ Source/Slash/Private/Items/Item.cpp
@@ -19,19 +19,31 @@ void AItem::BeginPlay()
 
 }
 
+float AItem::TransformedSin()
+{
+	return Amplitude * FMath::Sin(RunningTime * TimeConstant);
+}
+
+float AItem::TransformedCos()
+{
+	return Amplitude * FMath::Cos(RunningTime * TimeConstant);
+}
+
 // Called every frame
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	// Movement rate in units of cm/s 
-	float MovementRate = 50.f;
-	float RotationRate = 45.f;
+	RunningTime += DeltaTime;
 
-	// MovementRate * DeltaTime = (cm/s) * (s/frame) = (cm/frame)
-	AddActorWorldOffset(FVector(MovementRate * DeltaTime, 0.f, 0.f));
-	AddActorWorldRotation(FRotator(0.f, RotationRate * DeltaTime, 0.f));
+	//float DeltaZ = Amplitude * FMath::Sin(RunningTime * TimeConstant);
+
+	//AddActorWorldOffset(FVector(0.f, 0.f, DeltaZ));
+
 	DRAW_SPHERE_SINGLE_FRAME(GetActorLocation());
 	DRAW_VECTOR_SINGLE_FRAME(GetActorLocation(), GetActorLocation() + GetActorForwardVector() * 100.f);
+
+	FVector AvgVector = Avg<FVector>(GetActorLocation(), FVector::ZeroVector);
+	DRAW_POINT_SINGLE_FRAME(AvgVector);
 }
 
