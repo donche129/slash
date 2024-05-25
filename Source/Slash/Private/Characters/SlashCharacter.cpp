b0ed769e7b65bfa8a243744f1eb2477d851636f2
Donald Cherestal <donaldcherestal@gmail.com>
2024-05-25 11:58:28 -0400

    Lecture 86: Jumping

--- Source/Slash/Private/Characters/SlashCharacter.cpp
+++ Source/Slash/Private/Characters/SlashCharacter.cpp
@@ -135,5 +135,6 @@ void ASlashCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComp
 	PlayerInputComponent->BindAxis(FName("MoveRight"), this, &ASlashCharacter::MoveRight);
 	PlayerInputComponent->BindAxis(FName("Turn"), this, &ASlashCharacter::Turn);
 	PlayerInputComponent->BindAxis(FName("LookUp"), this, &ASlashCharacter::LookUp);*/
+	PlayerInputComponent->BindAction(FName("Jump"), IE_Pressed, this, &ACharacter::Jump);
 }
 
