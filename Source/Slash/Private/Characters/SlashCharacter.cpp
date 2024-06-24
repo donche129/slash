2774dfb5d833c3b81a1d37ac387beeb3727cc839
Donald Cherestal <donaldcherestal@gmail.com>
2024-06-23 21:46:37 -0400

    Lecture 229: Soul Drift

--- Source/Slash/Private/Characters/SlashCharacter.cpp
+++ Source/Slash/Private/Characters/SlashCharacter.cpp
@@ -199,6 +199,10 @@ void ASlashCharacter::EKeyPressed()
 	AWeapon* OverlappingWeapon = Cast<AWeapon>(OverlappingItem);
 	if (OverlappingWeapon)
 	{
+		if (EquippedWeapon)
+		{
+			EquippedWeapon->Destroy();
+		}
 		EquipWeapon(OverlappingWeapon);
 	}
 	else
